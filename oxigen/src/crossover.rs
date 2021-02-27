f8daab755b4af81f3b23be01fd590c5f3969fa75
Martin1887 <marcos.martin.pozo.delgado@gmail.com>
2021-02-27 18:44:39 +0100

    Fix uniform crossover with different length individuals and more descriptive error message in other crossovers when the smallest individual has length 0 or 1.

--- oxigen/src/crossover.rs
+++ oxigen/src/crossover.rs
@@ -23,10 +23,14 @@ pub enum CrossoverFunctions {
 }
 
 impl<T, G: Genotype<T>> Crossover<T, G> for CrossoverFunctions {
+    #[allow(clippy::comparison_chain)]
     fn cross(&self, ind1: &G, ind2: &G) -> (G, G) {
         match self {
             SingleCrossPoint => {
                 let ind_size = min(ind1.iter().len(), ind2.iter().len());
+                if ind_size <= 1 {
+                    panic!("The size of the smaller individual is 0 or 1, so no crosspoint can be generated in the middle of it");
+                }
                 let cross_point = SmallRng::from_entropy().sample(Uniform::from(1..ind_size));
 
                 (
@@ -58,10 +62,13 @@ impl<T, G: Genotype<T>> Crossover<T, G> for CrossoverFunctions {
             }
             MultiCrossPoint => {
                 let ind_size = min(ind1.iter().len(), ind2.iter().len());
+                if ind_size <= 1 {
+                    panic!("The size of the smaller individual is 0 or 1, so no crosspoint can be generated in the middle of it");
+                }
                 let mut cross_points = Vec::new();
                 let mut point_maximum = ind_size / 2;
                 if point_maximum <= 2 {
-                    point_maximum = 3;
+                    point_maximum = 3.min(ind_size);
                 }
                 let mut i = SmallRng::from_entropy().sample(Uniform::from(1..point_maximum));
                 while i < ind_size {
@@ -113,20 +120,54 @@ impl<T, G: Genotype<T>> Crossover<T, G> for CrossoverFunctions {
                         .collect(),
                 )
             }
-            UniformCross => (
-                ind1.clone()
+            UniformCross => {
+                let mut child1: G = ind1
+                    .clone()
                     .into_iter()
                     .zip(ind2.clone().into_iter())
                     .enumerate()
                     .map(|(i, (gen1, gen2))| if i % 2 == 0 { gen1 } else { gen2 })
-                    .collect(),
-                ind1.clone()
+                    .collect();
+                let mut child2: G = ind1
+                    .clone()
                     .into_iter()
                     .zip(ind2.clone().into_iter())
                     .enumerate()
                     .map(|(i, (gen1, gen2))| if i % 2 != 0 { gen1 } else { gen2 })
-                    .collect(),
-            ),
+                    .collect();
+
+                // If one individual is shorter than other the zipped iterator ends in the smallest
+                // one, so the rest of the original individual should be appended to the child
+                let len1 = ind1.iter().len();
+                let len2 = ind2.iter().len();
+                if len1 > len2 {
+                    child1 = child1
+                        .clone()
+                        .into_iter()
+                        .chain(
+                            ind1.clone()
+                                .into_iter()
+                                .enumerate()
+                                .filter(|(i, _el)| *i >= len2)
+                                .map(|(_i, el)| el),
+                        )
+                        .collect();
+                } else if len1 < len2 {
+                    child2 = child2
+                        .clone()
+                        .into_iter()
+                        .chain(
+                            ind2.clone()
+                                .into_iter()
+                                .enumerate()
+                                .filter(|(i, _el)| *i >= len1)
+                                .map(|(_i, el)| el),
+                        )
+                        .collect();
+                }
+
+                (child1, child2)
+            }
         }
     }
 }
